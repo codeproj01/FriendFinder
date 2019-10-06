6fbf9e7716031b93eebff9f507261f6d85b574d0
codeproj01 <cliftonmpls@comcast.net>
2019-10-05 22:46:12 -0500

    Updated initial deployment

---
+++ app/data/friends.js
@@ -0,0 +1,192 @@
+const friends = [ {
+  "name":"Ahmed",
+  "photo":"https://www.pexels.com/photo/photography-of-a-guy-wearing-green-shirt-1222271/",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},    {
+  "name":"caption1",
+  "photo":"https://www.pexels.com/photo/beard-blur-casual-close-up-462680/",
+  "scores": [
+      5,
+      2,
+      5,
+      4,
+      2,
+      5,
+      2,
+      1,
+      3,
+      3
+  ]
+},
+{
+  "name":"caption2",
+  "photo":"https://www.pexels.com/photo/close-up-photography-of-a-woman-near-wall-1065084/",
+  "scores": [
+      2,
+      3,
+      5,
+      4,
+      2,
+      5,
+      1,
+      1,
+      4,
+      3
+  ]
+},
+{
+  "name": "caption3",
+  "photo": "https://www.pexels.com/photo/man-young-happy-smiling-91227/",
+  "scores": [
+      3,
+      4,
+      5,
+      1,
+      2,
+      1,
+      4,
+      1,
+      2,
+      4
+  ]
+},
+{
+  "name": "caption4",
+  "photo": "https://www.pexels.com/photo/woman-wearing-white-sleeveless-shirt-and-white-denim-shorts-1082962/",
+  "scores": [
+      5,
+      5,
+      4,
+      2,
+      2,
+      2,
+      4,
+      1,
+      5,
+      4
+  ]
+},
+{
+  "name": "caption5",
+  "photo": "https://www.pexels.com/photo/boy-child-clouds-kid-346796/",
+  "scores": [
+      4,
+      2,
+      1,
+      3,
+      2,
+      3,
+      4,
+      1,
+      1,
+      4
+  ]
+},
+{
+  "name": "caption6",
+  "photo": "https://www.pexels.com/photo/four-women-standing-on-mountain-1574650/",
+  "scores": [
+      3,
+      5,
+      1,
+      4,
+      1,
+      4,
+      4,
+      1,
+      5,
+      1
+  ]
+},
+{
+  "name": "caption7",
+  "photo": "https://www.pexels.com/photo/woman-wearing-grey-long-sleeved-top-photography-1122868/",
+  "scores": [
+      2,
+      5,
+      2,
+      5,
+      2,
+      5,
+      4,
+      1,
+      3,
+      2
+  ]
+},
+{
+  "name": "caption8",
+  "photo": "https://www.pexels.com/photo/woman-sitting-and-smiling-1858175/",
+  "scores": [
+      1,
+      4,
+      3,
+      2,
+      3,
+      3,
+      4,
+      1,
+      5,
+      5
+  ]
+},
+{
+  "name": "caption9",
+  "photo": "https://www.pexels.com/photo/adult-african-american-afro-black-female-1181519/",
+  "scores": [
+      5,
+      1,
+      4,
+      3,
+      4,
+      2,
+      4,
+      1,
+      4,
+      3
+  ]
+},
+{
+  "name": "caption10",
+  "photo": "https://www.pexels.com/photo/two-man-and-two-woman-standing-on-green-grass-field-1162964/",
+  "scores": [
+      4,
+      2,
+      5,
+      1,
+      5,
+      3,
+      4,
+      1,
+      2,
+      5
+  ]
+},
+{
+  "name": "caption11",
+  "photo": "https://www.pexels.com/photo/person-on-a-bridge-near-a-lake-747964/",
+  "scores": [
+      5,
+      3,
+      4,
+      3,
+      2,
+      1,
+      4,
+      5,
+      2,
+      4
+  ]
+}];
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,40 @@
+let friends = require("../data/friends.js")
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+
+    function minIndexArr(Arr) {
+        return Arr.indexOf(Math.min.apply(Math, Arr));
+    }
+
+    app.post("/api/friends", function (req, res) {
+
+        let userScores = req.body.scores;
+	    console.log(userScores)
+        let scores = [];
+
+        for (let i = 0; i < friends.length; i++) {
+            let lowestScoreDiff = 0;
+            let highestScoreDiff = 40;
+            for (let j = 0; j < userScores.length; j++) {
+                lowestScoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j])));
+            }
+            scores.push(lowestScoreDiff)
+        }
+        console.log(scores)
+        let minIndexArrValue = minIndexArr(scores)
+        let userData = friends[minIndexArrValue]
+        console.log(minIndexArrValue)
+        friends.push(req.body);
+        res.json(userData);
+
+    });
+
+
+
+}
+// function to find the index of the smallest element from an array
