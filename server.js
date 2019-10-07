b027b1b368318f98e39f407433fbd41a26205002
codeproj01 <cliftonmpls@comcast.net>
2019-10-06 21:03:01 -0500

    update

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 // ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 const express = require("express");
 const path = require("path");
 
 
-// Tells node that we are creating an "express" server
+//express server
 var app = express();
-// Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8045;
+// Sets an initial port.
+const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+//Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,20 +18,8 @@ require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 
-
-// Serving our HTML files.
-// app.get("/survey", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./public/survey.html"));
-//   });
-
-
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./public/home.html"));
-//   });
-
 // // =============================================================================
 // // LISTENER
-// // The below code effectively "starts" our server
 // // =============================================================================
 
 app.listen(PORT, function() {
